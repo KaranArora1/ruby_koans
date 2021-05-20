8d84c81605f24d23810ccd9ca3e3b092d6ea33a5
karanaro <karanaro@umich.edu>
2021-05-20 12:49:20 -0400

    Complete about_triangle_project_2.rb (160/281)

--- triangle.rb
+++ triangle.rb
@@ -14,13 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c
+
+  if a <= 0 || b <= 0 || c <= 0 
+    raise TriangleError
+  elsif a + b <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  elsif a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c 
     :isosceles
   else 
     :scalene
   end
+
 end
 
 # Error class used in part 2.  No need to change this code.
